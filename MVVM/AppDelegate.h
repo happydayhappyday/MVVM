525c3df9164292a50db3878e878f674305fac0c6
xiayuanqun <xiayuanqun@xiayuanqundemacbook-pro.local>
2017-01-19 15:11:46 +0800

    Initial Commit

---
+++ MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by xiayuanqun on 17/1/19.
+//  Copyright © 2017年 xiayuanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM
+//
+//  Created by xiayuanqun on 17/1/19.
+//  Copyright © 2017年 xiayuanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
